0f530bfdfe7194670d058278e8bde9b320bf3871
kmcnee <kyle.mcnee@gmail.com>
2016-02-22 00:15:58 -0500

    Kyle McNee

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -5,21 +5,66 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  Button mButton;
+  EditText mEditText;
+  TextView mAnimatedWish;
+  ArrayList<String> mWishArray;
+  ListView mWishList;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    //TODO: set up all the view and event listeners.
+    mButton = (Button)findViewById(R.id.button);
+    mEditText = (EditText)findViewById(R.id.editText);
+    mAnimatedWish = (TextView)findViewById(R.id.textView);
+    mWishList = (ListView)findViewById(R.id.listView);
+
+    mWishArray = new ArrayList<String>();
+
+
+
+    mButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String wish = mEditText.getText().toString();
+        mWishArray.add(wish);
+        mAnimatedWish.setText(wish);
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, mWishArray);
+        mWishList.setAdapter(adapter);
+          Collections.reverse(mWishArray);
+
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
+        mAnimatedWish.startAnimation(animation);
+        mEditText.setText(null);
+
+        Toast.makeText(MainActivity.this, "Your wish is my command", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+
+    // set up all the view and event listeners.
   }
 
-  @Override
   public void onClick(View v) {
-    // TODO: 1. get the text from the input field
+
+
+    //       1. get the text from the input field
     //       2. animate it in the center of the screen
     //       3. add it to the list wish
     //       4. clear the input field
